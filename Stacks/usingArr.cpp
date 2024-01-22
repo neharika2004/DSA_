cfdc8587454028f4fa00c9a8939fb5ef08648b76
neharika2004 <125340101+neharika2004@users.noreply.github.com>
2024-01-22 12:14:15 +0530

    Add files via upload

---
+++ Stacks/usingArr.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+    public:
+       int top;
+       int*arr;
+       int size;
+
+    Stack(int size){
+        this->size=size;
+        arr=new int(size);
+        top=-1;
+    }
+
+    void push(int element){
+    if(size-top > 1){
+        top++ ;
+        arr[top]=element;
+    }
+    else{
+        cout<<"Stack Overflow!"<<endl;
+    }
+    }
+    void pop() {
+        if(top>=0){
+            top-- ;
+        }
+        else{
+            cout<<"Stack Underflow"<<endl;
+        }
+    }
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int peek(){
+        if(top>=0){
+           return arr[top];
+        }
+        else{
+            cout<<"Stack Underflow!"<<endl;
+            return -1;
+        }
+    }
+};
+
+
+int main(){
+    Stack st(5);
+
+    st.push(22);
+    st.push(43);
+    st.push(44);
+
+    cout<< st.peek() <<endl;
+    st.pop();
+    cout<< st.peek() <<endl;
+    st.pop();
+    cout<< st.peek() <<endl;
+    // st.pop();
+    // cout<< st.peek() <<endl;
+
+    if(st.isEmpty()){
+        cout<<"Stack is empty!"<<endl;
+    }
+    else{
+        cout<<"Stack is not empty!"<<endl;
+    }
+}
